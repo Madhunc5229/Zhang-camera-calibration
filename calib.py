e2273fadc938ecc912696b56c89af9771aac2646
Madhunc5229 <madhunc117@gmail.com>
2023-04-11 18:02:35 -0400

    optimized K

--- calib.py
+++ calib.py
@@ -1,10 +1,11 @@
 from utils import *
 from computation import *
 import glob
-
+from scipy import optimize
 
 
 def main():
+
     data_path = 'Calibration_Imgs/'
     img_paths = sorted(glob.glob(data_path+'*.jpg'))
     
@@ -23,7 +24,24 @@ def main():
         RT = getExtrinsicMat(h,K_init)
         E_init.append(RT)
 
-    
+    kC = (0,0)
+    f.write('Distortion Coordinates before optimization: '+ str(np.round(kC,5))+ '\n')
+    reprojection_error = estimateReprojectionError(K_init, kC, (M_pts, m_pts, E_init))
+    print("Projection error before optimization, : " + str(reprojection_error))
+    f.write('Projection error before optimization : '+ str(np.round(reprojection_error,5))+ '\n\n\n')
+
+    alpha, beta, gamma = K_init[0, 0], K_init[1, 1], K_init[0, 1]
+    u0,v0 = K_init[0, 2], K_init[1, 2]
+    k1,k2 = 0, 0
+
+    init_params = [alpha, beta, gamma, u0, v0, k1, k2]
+
+    out = optimize.least_squares(fun = loss, x0 = init_params, method="lm", args = [M_pts, m_pts, E_init])
+    optimal_params = out.x
+
+    kC = (optimal_params[-2], optimal_params[-1])
+    K = K_matrix(optimal_params)
+    f.write('optimized K matrix : \n '+ str(np.matrix.round(K,3))+ '\n\n')
 
 
 if __name__ == "__main__":
--- computation.py
+++ computation.py
@@ -42,6 +42,12 @@ def getIntrinsicMat(H_matrices):
     return K
 
 
+def K_matrix(params):
+    alpha, beta, gamma, u0, v0, _ , _ = params
+    return np.array([[alpha, gamma, u0],
+                  [0,     beta,  v0],
+                  [0,     0,      1]], dtype = np.float64)
+
 def getExtrinsicMat(H, K):
 
     h1,h2,h3 = H.T 
@@ -55,3 +61,88 @@ def getExtrinsicMat(H, K):
     
     return np.stack((r1,r2,r3,t), axis=1)
 
+def estimateReprojectionError(K,kC, data):
+    M_pts, m_pts, Extrinsics = data
+    errors = []
+    for i,RT in enumerate(Extrinsics):
+        # estimate reprojection error for all points in given image
+#         e = reProjectionError(M_pts[i], m_pts[i], K, RT, kC)
+        e = reProjectionError(M_pts[i], m_pts[i], K, RT, kC)
+        errors.append(e) 
+    return np.mean(errors)
+
+def reProjectionError(M_i, m_i, K, RT, kC):
+
+    r1,r2,r3,t = RT.T
+    R = np.stack((r1,r2,r3), axis=1)
+    t = t.reshape(-1,1)
+
+    ones = np.ones(len(m_i)).reshape(-1,1)
+    zeros=  np.zeros(len(m_i)).reshape(-1,1)
+
+    
+    m_i_ = projectPoints(M_i, RT, K, kC)
+
+    error = []            
+    for m, m_ in  zip(m_i, m_i_.squeeze()):
+        e_ = np.linalg.norm(m - m_, ord=2) # compute L2 norm
+        error.append(e_)
+        
+    return np.mean(error)
+
+def projectPoints(M_i, RT, K, kC):
+    u0,v0 = K[0,2], K[1,2]
+    alpha,beta = K[0,0], K[1,1]
+    k1,k2 = kC    
+    m_i_ = []
+    for M in M_i:
+        M = np.float64(np.hstack((M,0,1))) # make image and world points as homogenous coordinates        
+        # project point from world points at extrinsic level - only apply RT
+        xy_ = RT.dot(M) 
+        xy_ = xy_/xy_[-1]
+#         xy_ = [alpha*xy_[0]/xy_[2], beta*xy_[1]/xy_[2], 1]
+#        project point from extrinsic level image points to intrinsic level        
+        U = K.dot(xy_)
+        U = U/U[-1]
+
+        u,v = U[0], U[1]
+        x, y = xy_[0], xy_[1]
+
+        r_sq = x**2 + y**2
+        u_ = u + (u - u0)*(k1* r_sq + k2*(r_sq**2))
+        v_ = v + (v - v0)*(k1* r_sq + k2*(r_sq**2))
+
+        # obtain projected point after applying distortion kC
+        m_ = np.hstack((u_,v_))
+        m_i_.append(m_)
+    return np.array(m_i_)
+
+def loss(params, M_pts, m_pts, Extrinsics):
+    #     l2error works faster when you have 13 (no. of images) residuals.
+    kC = (params[-2], params[-1])
+    K = K_matrix(params)
+    error = []
+    for i,RT in enumerate(Extrinsics):    
+        # estimate error for all points in given image
+        e = geometricError(m_pts[i], M_pts[i], K, RT, kC)
+        error = np.hstack((error,e))
+    print(len(error))
+    return error
+
+def geometricError(m_i, M_i, K, RT, kC):
+
+    r1,r2,r3,t = RT.T
+    R = np.stack((r1,r2,r3), axis=1)
+    t = t.reshape(-1,1)
+    ones = np.ones(len(m_i)).reshape(-1,1)
+    zeros=  np.zeros(len(m_i)).reshape(-1,1)
+
+    
+    m_i_ = projectPoints(M_i, RT, K, kC)
+
+    error = [] 
+    for m, m_ in  zip(m_i, m_i_.squeeze()):
+        e_ = np.linalg.norm(m - m_, ord=2) # compute L2 norm
+        error.append(e_)
+         
+    return np.sum(error) # sum the errors as given in the paper
